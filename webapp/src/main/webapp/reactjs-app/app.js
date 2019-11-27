2e102426f6e72c4aec466c4470cfddefd7c74d55
Peter Nedonosko <pnedonosko@exoplatform.com>
2019-11-28 00:06:19 +0200

    ReactJS based Tip app

---
+++ webapp/src/main/webapp/reactjs-app/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../css/app.less";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="tip-app">
+        <h1> Hello, World! Here will be a Tip Of The Day :)</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
